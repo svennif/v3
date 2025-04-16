7a67862f6eb88b806322e4d0e0c5c1220e2f2cd8
Sven Ingar Frantzen <sven.ingar@gmail.com>
2025-04-16 21:52:53 +0200

    1

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,11 @@
+export default function ProductCard() {
+  return (
+    <li className="project-container">
+      <div className="project-image"></div>
+      <div className="project-title"></div>
+      <div className="project-desc"></div>
+      <div className="project-tags"></div>
+      <div className="project-button"></div>
+    </li>
+  )
+}
---
+++ components/ProjectList.tsx
@@ -0,0 +1,9 @@
+import ProjectCard from './ProjectCard';
+
+export default function ProjectList() {
+  return (
+    <ul className='columns-3'>
+      <ProjectCard />
+    </ul>
+  )
+}
