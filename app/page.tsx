592bfbf9d6834d7083e4b18cb6d832d945a247fa
Sven Ingar Frantzen <sven.ingar@gmail.com>
2025-04-19 10:34:59 +0200

    refactor: migrate from Next.js to Vite and React
    - Updated package.json to replace Next.js with Vite, adjusting scripts accordingly.
    - Removed PostCSS configuration as Tailwind CSS is no longer used.
    - Deleted unused SVG and image files from the public directory.
    - Removed TypeScript configuration file (tsconfig.json) as TypeScript is not utilized.
    - Added ESLint configuration for improved code quality.
    - Created a new index.html file for Vite setup.
    - Added new SVG assets for Vite and React logos.
    - Implemented App component with basic functionality and styling.
    - Created main entry point (main.jsx) for rendering the App component.
    - Established global styles in index.css for consistent theming.
    - Configured Vite for React application.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductList from "@/components/ProjectList";
-
-export default function Home() {
-  return (
-    <div className="max-w-5xl mx-auto">
-      <ProductList />
-    </div>
-  )
-}
--- components/ProjectList.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectCard from './ProjectCard';
-
-export default function ProjectList() {
-  return (
-    <ul className='columns-3'>
-      <ProjectCard />
-    </ul>
-  )
-}
